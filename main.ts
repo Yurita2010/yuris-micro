58d5d497c0533704e99da8c59ad6bf3c07ff36e2
Yurita2010 <123651093+Yurita2010@users.noreply.github.com>
2023-02-10 15:04:11 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    getal += 1 + getal
 })
+input.onButtonPressed(Button.B, function () {
+    getal += 1 - getal
+})
+let getal = 0
+getal = 0
